4c1718ecce82e0974ebb1d14910bbc457aa77794
George <gwmkwan@gmail.com>
2023-05-09 11:13:29 -0600

    feat: create header

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
+import React, { useEffect, useState } from 'react';
+
 import '@/styles/globals.scss';
 
+import { Layout } from '../components';
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
