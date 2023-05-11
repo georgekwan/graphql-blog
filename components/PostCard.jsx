585c94a444c67481f37ce6a5d6ad76db1c2c24de
George <gwmkwan@gmail.com>
2023-05-11 15:01:32 -0600

    feat: add feature image

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -8,12 +8,13 @@ const PostCard = ({ post }) => {
   return (
     <div className="bg-white shadow-lg rounded-lg p-0 lg:p-8 pb-12 mb-8">
       <div className="relative overflow-hidden shadow-md pb-80 mb-6">
-        {/* <img
+        <img
           src={post.featuredImage.url}
           alt={post.title}
           className="object-top absolute h-80 w-full object-cover  shadow-lg rounded-t-lg lg:rounded-lg"
-        /> */}
+        />
       </div>
+      <h1></h1>
     </div>
   );
 };
