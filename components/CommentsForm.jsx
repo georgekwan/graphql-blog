84170e20ceea83750f8e29f3fdcaca3f9a6004d2
George <gwmkwan@gmail.com>
2023-05-14 09:54:37 -0600

    feat: setup comment form

--- components/CommentsForm.jsx
+++ components/CommentsForm.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 
-const CommentsForm = () => {
+const CommentsForm = ({ slug }) => {
+  const [error, setError] = useState(false);
+  const [localStorage, setLocalStorage] = useState(null);
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
+  const commentEl = useRef();
+  const nameEl = useRef();
+  const emailEl = useRef();
+  const storeDataEl = useRef();
+
   return (
-    <div>
-      <h1>CommentsForm</h1>
+    <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
+      <h3 className="text-xl mb-8 font-semibold border-b pb-4">
+        Leave a Reply
+      </h3>
+      <div className="grid grid-cols-1 gap-4 mb-4"></div>
+      <div className="grid grid-cols-1 gap-4 mb-4"></div>
+      <div className="grid grid-cols-1 gap-4 mb-4"></div>
     </div>
   );
 };
