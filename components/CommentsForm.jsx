a45b6f4f495a391b010b9f076a49d71bb75e9632
George <gwmkwan@gmail.com>
2023-05-14 10:04:13 -0600

    feat: add comment box

--- components/CommentsForm.jsx
+++ components/CommentsForm.jsx
@@ -14,7 +14,14 @@ const CommentsForm = ({ slug }) => {
       <h3 className="text-xl mb-8 font-semibold border-b pb-4">
         Leave a Reply
       </h3>
-      <div className="grid grid-cols-1 gap-4 mb-4"></div>
+      <div className="grid grid-cols-1 gap-4 mb-4">
+        <textarea
+          ref={commentEl}
+          className="p-4 outline-none w-full rounded-lg h-40 focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700"
+          name="comment"
+          placeholder="Comment"
+        />
+      </div>
       <div className="grid grid-cols-1 gap-4 mb-4"></div>
       <div className="grid grid-cols-1 gap-4 mb-4"></div>
     </div>
